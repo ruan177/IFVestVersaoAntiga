4e7c0893b8af491e953ddb5d24768c9621c000fb
Ruan Patrick <114404700+ruan177@users.noreply.github.com>
2025-03-28 07:45:34 -0300

    Update 20250325144846-create-session-table.js

--- migrations/20250325144846-create-session-table.js
+++ migrations/20250325144846-create-session-table.js
@@ -6,31 +6,29 @@ module.exports = {
       sid: {
         type: Sequelize.STRING,
         primaryKey: true,
-        allowNull: false
       },
-      sess: { // Nome exato exigido pelo middleware
-        type: Sequelize.JSONB,
-        allowNull: false
+      userId: {
+        type: Sequelize.STRING,
+        allowNull: true,
       },
-      expire: { // Nome exato exigido pelo middleware
+      expires: {
         type: Sequelize.DATE,
-        allowNull: false
+        allowNull: false,
+      },
+      data: {
+        type: Sequelize.TEXT,
+        allowNull: false,
       },
       createdAt: {
         type: Sequelize.DATE,
         allowNull: false,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
       updatedAt: {
         type: Sequelize.DATE,
         allowNull: false,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      }
-    });
-
-    // Índice para expiração
-    await queryInterface.addIndex('sessions', ['expire'], {
-      where: { expire: { [Sequelize.Op.gt]: Sequelize.literal('CURRENT_TIMESTAMP') } }
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
     });
   },
 
